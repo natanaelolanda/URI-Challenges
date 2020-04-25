a85ecccba4c475893c92915a738cbd6872461b07
natanaelolanda <ntolanda@gmail.com>
2020-04-24 21:43:34 -0300

    Decifrando a Carta Criptografada atualizado

--- Decifrando-a-Carta-Criptografada.py
+++ Decifrando-a-Carta-Criptografada.py
@@ -1,29 +1,36 @@
+# TITULO: Decifrando a Carta Criptografada
+# CÓDIGO: 2502
 
-dados = input().split()
-c = int(dados[0])
-n = int(dados[1])
+while True:
+    try:
+        dados = input().split()
+        tamanho_cifras = int(dados[0])
+        qtd_textos_criptografados = int(dados[1])
 
-chave1 = input().lower()
-chave2 = input().lower()
+        chave1 = input().lower()
+        chave2 = input().lower()
 
-for x in range(n):
-    texto_criptografado = input()
-    texto_descriptografado = ""
+        for x in range(qtd_textos_criptografados):
+            texto_criptografado = input()
+            texto_descriptografado = ""
 
-    for letra in texto_criptografado:
-        letra2 = letra.lower()
+            for letra1 in texto_criptografado:
+                letra2 = letra1.lower()
 
-        if letra2 in chave1:
-            posicao = chave1.find(letra2)
-            letra2 = chave2[posicao]
+                if letra2 in chave1:
+                    posicao = chave1.find(letra2)
+                    letra2 = chave2[posicao]
 
-        elif letra2 in chave2:
-            posicao = chave2.find(letra2)
-            letra2 = chave1[posicao]
+                elif letra2 in chave2:
+                    posicao = chave2.find(letra2)
+                    letra2 = chave1[posicao]
 
-        if letra.isupper():
-            letra2 = letra2.upper()
+                if letra1.isupper():
+                    letra2 = letra2.upper()
 
-        texto_descriptografado += letra2
+                texto_descriptografado += letra2
 
-    print(texto_descriptografado)
+            print(texto_descriptografado)
+        print()
+    except EOFError:
+        break
